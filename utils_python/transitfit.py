1f80bab1852437ae13e03fe3fa5b637e6207ba51
alexis <roya1712@usherbrooke.ca>
2025-06-06 11:53:11 -0400

    Changed fitting to fit only free parameters

--- utils_python/transitfit.py
+++ utils_python/transitfit.py
@@ -2,60 +2,69 @@ import numpy as np
 from scipy.optimize import least_squares
 from utils_python.transitmodel import transitModel
 
-def fitTransitModel(sol, time, flux, ferror, itime):
+def createBounds(time, id_to_fit):
+    """
+    Creates the bounds for the parameters
+    """
     min_t = min(time)
     max_t = max(time)
 
-    bounds = ([1e-4, 0, -1, 0, 0, 0, -np.inf, -np.inf, min_t, min_t, 0, 0, -1, -1, -np.inf, -np.inf, -np.inf, -np.inf],
-              [1e3, 2, 1, 1, 1, 1, np.inf, np.inf, max_t, max_t, 2, 1, 1, 1, np.inf, np.inf, np.inf, np.inf])
+    lower_bound = np.array([1e-4, 0, -1, 0, 0, 0, -np.inf, -np.inf, min_t, min_t, 0, 0, -1, -1, -np.inf, -np.inf, -np.inf, -np.inf])
+    upper_bound = np.array([1e3, 2, 1, 1, 1, 1, np.inf, np.inf, max_t, max_t, 2, 1, 1, 1, np.inf, np.inf, np.inf, np.inf])
 
-    res = least_squares(transitToOptimize, sol, bounds=bounds, args=(time, flux, ferror, itime))
+    return (lower_bound[id_to_fit], upper_bound[id_to_fit])
 
-    return res.x
+def fitTransitModel(sol, time, flux, ferror, itime):
+    """
+    Function to call for fitting
+    """
+
+    # Fit only the parameters in id_to_fit
+    id_to_fit = np.array([0, 8, 10, 11])
+    sol_full = np.copy(sol) # Copy the initial guess
+    def wrapperTransit(sol_free, time, flux, ferror, itime):
+        for i, id in enumerate(id_to_fit):
+            sol_full[id] = sol_free[i]
+        return transitToOptimize(sol_full, time, flux, ferror, itime)
+    
+    bounds = createBounds(time, id_to_fit)
+
+    res = least_squares(wrapperTransit, sol[id_to_fit], bounds=bounds, args=(time, flux, ferror, itime))
+
+    # Recreate the solution with the fixed params
+    sol_full = np.copy(sol)
+    for i, id in enumerate(id_to_fit):
+        sol_full[id] = res.x[i]
+
+    return sol_full
 
 def transitToOptimize(sol, time, flux, ferror, itime):
-
-    # For now, we manually set the parameters we don't want to fit
-    # sol[0]  = 0.8  # Mean stellar density (g/cm^3)
-    sol[1]  = 0.0  # Only used for non-linear limb-darkening
-    sol[2]  = 0.0  # Only used for non-linear limb-darkening
-    sol[3]  = 0.6  # q1 (limb-darkening)
-    sol[4]  = 0.4  # q2 (limb-darkening)
-    sol[5]  = 0.0  # dilution
-    sol[6]  = 0.0  # Velocity offset
-    sol[7]  = 0.0  # photometric zero point
-    # sol[8]  = 0.449820           # Center of transit time (days)
-    sol[9]  = 2.483415           # Orbital Period (days)
-    # sol[10] = 0.5                     # Impact parameter
-    # sol[11] = 0.0263 # Rp/R*
-    sol[12] = 0.0  # sqrt(e)cos(w)
-    sol[13] = 0.0  # sqrt(e)sin(w)
-    sol[14] = 0.0  # RV amplitude (m/s)
-    sol[15] = 0.0  # thermal eclipse depth (ppm)
-    sol[16] = 0.0  # Ellipsodial variations (ppm)
-    sol[17] = 0.0  # Albedo amplitude (ppm)
+    """
+    Handles constraints and returns the vector of differences
+    """
+    n = len(time)
 
     # Parameter constraints (Return a big number if constraint is not respected)
     b = sol[10]
     Rp_Rs = sol[11]
     if b > 1 + Rp_Rs:
-        return 1e20
+        return np.full(n, 1e20)
     
     c1, c2, c3, c4 = sol[1], sol[2], sol[3], sol[4]
     # Quadratic coefficients
     if (c3 == 0 and c4 == 0):
         if not (0 < c1 < 2) or not (-1 < c2 < 1):
-            return 1e20
+            return np.full(n, 1e20)
 
     # Kipping coefficients
     elif (c1 == 0 and c2 == 0):
         if not (0 < c3 < 1) or not (0 < c4 < 1):
-            return 1e20
+            return np.full(n, 1e20)
     
     # Non linear law
     else:
         if not (0 < c1 < 1) or not (0 < c2 < 1) or not (0 < c3 < 1) or not (0 < c4 < 1):
-            return 1e20
+            return np.full(n, 1e20)
 
     y_model = transitModel(sol, time, itime=itime)
 
