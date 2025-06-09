4afba539e7e64f2088c5626adaab3b094ee0eb18
alexis <roya1712@usherbrooke.ca>
2025-06-09 15:35:37 -0400

    Added print params function

--- utils_python/transitplot.py
+++ utils_python/transitplot.py
@@ -44,3 +44,41 @@ def plotTransit(time, flux, sol, itime, nintg=41):
     plt.ylabel('Relative Flux') #y-label
     plt.axis((-1.5*tdur, 1.5*tdur, y1, y2))
     plt.show()
+
+def printParams(sol, ind_to_print=[]):
+    """
+    Prints the parameters in a nice way.
+    You can select the indices to print using a list or array.
+    """
+
+    paramsDict = {
+        "ρ (g/cm³)": sol[0], "c1": sol[1], "c2": sol[2], "q1": sol[3], "q2": sol[4],
+        "Dilution": sol[5], "Velocity Offset": sol[6], "Photometric zero point": sol[7],
+        "t0 (days)": sol[8], "Period (days)": sol[9], "Impact parameter": sol[10], "Rp/R*": sol[11],
+        "sqrt(e)cos(w)": sol[12], "sqrt(e)sin(w)": sol[13], "RV Amplitude (m/s)": sol[14],
+        "Thermal eclipse depth (ppm)": sol[15], "Ellipsoidal variations (ppm)": sol[16], "Albedo amplitude (ppm)": sol[17]
+    }
+
+    # Select only certain keys if specified
+    if len(ind_to_print) != 0:
+        dictToPrint = {}
+        for i, key in enumerate(paramsDict):
+            if i in ind_to_print:
+                dictToPrint[key] = paramsDict[key]
+    else:
+        dictToPrint = paramsDict
+
+    # Print every value in the dictionary
+    for key in dictToPrint:
+        val = paramsDict[key]
+        if val != 0:
+            exponent = np.floor(np.log10(abs(val)))
+        else:
+            exponent = 1
+
+        if abs(exponent) > 2:
+            print(f"{key + ':':<30} {val:>10.3e}")
+        elif len(str(val)) > 7:
+            print(f"{key + ':':<30} {val:>10.7f}")
+        else:
+            print(f"{key + ':':<30} {val:>10}")
