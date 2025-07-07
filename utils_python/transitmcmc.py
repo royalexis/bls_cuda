1c04ce00adcba773084139937ab8bc8b52d38536
alexis <roya1712@usherbrooke.ca>
2025-07-07 10:46:44 -0400

    Changed prior function + updated notebook with demcmc

--- utils_python/transitmcmc.py
+++ utils_python/transitmcmc.py
@@ -1,9 +1,9 @@
 import numpy as np
 import utils_python.transitmodel as transitm
 
-def mcmcTransitModel(sol, params_to_fit):
+def genmcmcInput(sol, params_to_fit):
     """
-    Returns the new log function and the sol array to use for mcmc
+    Returns the new log function, the sol array and the beta array to use for mcmc
 
     sol: Transit Model object containing the initial parameters
     params_to_fit: List containing strings of the names of the parameters to fit according to the tm class
@@ -67,25 +67,20 @@ def loglikehood(modelFunc, sol, time, flux, ferror, itime):
     return ll
 
 def logprior(sol, time):
-    badprior = -1e30
+    badprior = -np.inf
     lprior = 0
 
     min_t = min(time)
     max_t = max(time)
 
-    rho = sol[0]
-    nl1, nl2, nl3, nl4 = sol[1], sol[2], sol[3], sol[4]
-    dil, vof, zpt = sol[5], sol[6], sol[7]
+    # Todo: Change to handle multiple planets
+    ubounds = [1e3, 2, 1, 1, 1, 1, 1, 5, max_t, max_t, 2, 1, 1, 1, 5, 1e3, 1e3, 1e4]
+    lbounds = [1e-4, 0, -1, 0, 0, 0, 0, -5, min_t, min_t, 0, 0, -1, -1, -5, 0, -1e3, 0]
 
-    t0, per, bb, rdr = sol[8], sol[9], sol[10], sol[11]
-    ecw, esw, krv, ted, ell, alb = sol[12], sol[13], sol[14], sol[15], sol[16], sol[17]
-
-    cond1 = 1e-4 < rho < 1e3 and 0 <= nl1 <= 2 and -1 <= nl2 <= 1 and 0 <= nl3 <= 1 and 0 <= nl4 <= 1 and 0 <= dil <= 1 and 0 <= vof < 1 and -5 < zpt < 5
-    cond2 = min_t < t0 < max_t and min_t < per < max_t and 0 <= bb < 2 and 0 <= rdr <= 1 and -1 <= ecw <= 1 and -1 <= esw <= 1
-    # Unsure about what priors I should put for the last 4 params
-    cond3 = -5 < krv < 5 and 0 <= ted < 1e3 and -1e3 < ell < 1e3 and 0 <= alb < 1e4
-
-    if not (cond1 and cond2 and cond3):
-        lprior = badprior
+    for i in range(len(sol)):
+        if lbounds[i] <= sol[i] <= ubounds[i]:
+            continue
+        else:
+            return badprior
     
     return lprior
